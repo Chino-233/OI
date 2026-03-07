2b0ea0ff91b930df67ec8ea6cf034d0c880a1c4c
Chino-233 <2023192044@email.szu.edu.cn>
2026-03-08 01:28:00 +0800

    P1403

---
+++ luogu/数学Ⅰ/P1403_倍数思想.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// const int N = 1e6 + 10;
+const int N = 10;
+#define ll long long
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    #endif
+    ll n;
+    cin>>n;
+    ll ans=0;
+    for(int i=1;i<=n;i++) ans+=n/i;
+    cout<<ans;
+    return 0;
+}
