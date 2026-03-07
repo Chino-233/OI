c8a922148e3b91e43412d50eb3245b3d6d7acb83
Chino-233 <2023192044@email.szu.edu.cn>
2026-03-08 01:27:32 +0800

    P4057

---
+++ luogu/P4057.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// const int N = 1e6 + 10;
+const int N = 10;
+#define ll long long
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    #endif
+    ll a,b,c;
+    cin>>a>>b>>c;
+    cout<<lcm(lcm(a,b),c);
+    return 0;
+}
